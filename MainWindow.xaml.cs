bef484647d2100be079d5c0798f64bf2bf961c6b
Andrey Simanov <griffy@mail.ru>
2024-08-07 03:39:02 +0300

    Small refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,104 +3,89 @@ using System.Threading.Channels;
 using System.Windows;
 using TelephoneExchange.Models;
 
-namespace TelephoneExchange
+namespace TelephoneExchange;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    readonly Channel<Call> channel = Channel.CreateUnbounded<Call>();
+    readonly ObservableCollection<Agent> agents = new();
+    readonly ObservableCollection<Call> incomingCalls = new();
+    readonly ObservableCollection<Call> processedCalls = new();
+
+    public MainWindow()
     {
-        readonly Channel<Call> channel = Channel.CreateUnbounded<Call>();
-        readonly ObservableCollection<Agent> agents = new();
-        readonly ObservableCollection<Call> incomingCalls = new();
-        readonly ObservableCollection<Call> processedCalls = new();
+        InitializeComponent();
+        callsListView.ItemsSource = incomingCalls;
+        processedCallsListView.ItemsSource = processedCalls;
+        agentslistView.ItemsSource = agents;
+        StartCalling();
+    }
 
-        public MainWindow()
+    void AddAgentClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(agentNameTextBox.Text))
+            return;
+
+        string agentName = agentNameTextBox.Text.Trim();
+        if (agents.Any(item => string.Equals(item.Name, agentName)))
         {
-            InitializeComponent();
-            callsListView.ItemsSource = incomingCalls;
-            processedCallsListView.ItemsSource = processedCalls;
-            agentslistView.ItemsSource = agents;
-            StartCalling();
+           MessageBox.Show($"Agent {agentName} already exists, please choose another name.",
+                                  "Error",
+                                  MessageBoxButton.OK,
+                                  MessageBoxImage.Error);
+            return;
         }
+        AddAgent(agentName);
+    }
 
-        void AddAgentClick(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(agentNameTextBox.Text))
-                return;
-
-            string agentName = agentNameTextBox.Text.Trim();
-            if (agents.Any(item => string.Equals(item.Name, agentName)))
+    void RemoveAgentClick(object sender, RoutedEventArgs e)
+    {
+        if (agentslistView.SelectedItem != null)
+        { 
+            var agent = agentslistView.SelectedItem as Agent;
+            if (agent != null)
             {
-               MessageBox.Show($"Agent {agentName} already exists, please choose another name.",
-                                      "Error",
-                                      MessageBoxButton.OK,
-                                      MessageBoxImage.Error);
-                return;
-            }
-            AddAgent(agentName);
-        }
-
-        void RemoveAgentClick(object sender, RoutedEventArgs e)
-        {
-            if (agentslistView.SelectedItem != null)
-            { 
-                var agent = agentslistView.SelectedItem as Agent;
-                if (agent != null)
-                {
-                    agents.Remove(agent);
-                    agent.Dispose();
-                }
+                agents.Remove(agent);
+                agent.Dispose();
             }
         }
+    }
 
-        async void AddCallClick(object sender, RoutedEventArgs e) => await AddCall();
+    async void AddCallClick(object sender, RoutedEventArgs e) => await AddCall();
 
-        void StartCalling()
+    void StartCalling()
+    {
+        Task.Factory.StartNew(async () =>
         {
-            Task.Factory.StartNew(async () =>
+            var rnd = new Random();
+            while (true)
             {
-                var rnd = new Random();
-                while (true)
-                {
-                    await Task.Delay(rnd.Next(1000, 2000));
-                    await AddCall();
-                }
-            },
-            CancellationToken.None,
-            TaskCreationOptions.None,
-            TaskScheduler.FromCurrentSynchronizationContext());
-        }
+                await Task.Delay(rnd.Next(1000, 2000));
+                await AddCall();
+            }
+        },
+        CancellationToken.None,
+        TaskCreationOptions.None,
+        TaskScheduler.FromCurrentSynchronizationContext());
+    }
 
-        void AddAgent(string agentName)
-        {
-            var agent = new Agent(agentName, new CancellationTokenSource());
-            agents.Add(agent);
-            Task.Factory.StartNew(async () =>
-            {
-                var token = agent.Cts.Token;
-                while (!token.IsCancellationRequested)
-                {
-                    var call = await channel.Reader.ReadAsync(token);
-                    if (call == null)
-                        break;
+    void AddAgent(string agentName)
+    {
+        var agent = new Agent(agentName);
+        agents.Add(agent);
+        agent.ProcessCalls(channel, ProcessedСallback, TaskScheduler.FromCurrentSynchronizationContext());
+    }
 
-                    await agent.ProcessCall(call);
+    void ProcessedСallback(Call call)
+    {
+        incomingCalls.Remove(call);
+        processedCalls.Add(call);
+    }
 
-                    incomingCalls.Remove(call);
-                    processedCalls.Add(call);
-                }
-            },
-           CancellationToken.None,
-           TaskCreationOptions.None,
-           TaskScheduler.FromCurrentSynchronizationContext());
-        }
-
-        ValueTask AddCall()
-        {
-            var call = new Call(Guid.NewGuid());
-            incomingCalls.Add(call);
-            return channel.Writer.WriteAsync(call);
-        }
+    ValueTask AddCall()
+    {
+        var call = new Call(Guid.NewGuid());
+        incomingCalls.Add(call);
+        return channel.Writer.WriteAsync(call);
     }
 }
--- Models/Agent.cs
+++ Models/Agent.cs
@@ -1,20 +1,36 @@
-﻿namespace TelephoneExchange.Models;
+﻿using System.Threading.Channels;
 
-public record Agent(string Name, CancellationTokenSource Cts) : IDisposable
+namespace TelephoneExchange.Models;
+
+public record Agent(string Name) : IDisposable
 {
-    readonly Random rnd = new ();
+    readonly Random rnd = new();
+    readonly CancellationTokenSource cts = new CancellationTokenSource();
 
-    public async Task  ProcessCall(Call call)
-    {
-        var durationSec = rnd.Next(1000, 5000);
-        await Task.Delay(durationSec);
-        call.AgentName = Name;
-        call.DurationSec = durationSec;
-    }
+    public void ProcessCalls(Channel<Call> сhannel, Action<Call> callback, TaskScheduler taskScheduler) =>
+        Task.Factory.StartNew(async () =>
+        {
+            var token = cts.Token;
+            while (!token.IsCancellationRequested)
+            {
+                var call = await сhannel.Reader.ReadAsync(token);
+                if (call == null)
+                    break;
+
+                var durationSec = rnd.Next(1000, 5000);
+                await Task.Delay(durationSec);
+                call.AgentName = Name;
+                call.DurationSec = durationSec;
+                callback?.Invoke(call);
+            }
+        },
+        CancellationToken.None,
+        TaskCreationOptions.None,
+        taskScheduler);
 
     public void Dispose()
     {
-        Cts.Cancel();
-        Cts.Dispose();
+        cts.Cancel();
+        cts.Dispose();
     }
 }
